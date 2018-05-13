99e4cccfe358087484138181704f17d52d92a274
Aditya Parulekar <adityaparulekar@Adityas-MBP.attlocal.net>
2018-05-13 08:03:52 -0500

    Second Time

--- MyChatApp/app/src/main/java/com/example/adityaparulekar/mychatapp/MainActivity.java
+++ MyChatApp/app/src/main/java/com/example/adityaparulekar/mychatapp/MainActivity.java
@@ -1,23 +1,42 @@
 package com.example.adityaparulekar.mychatapp;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar mToolbar;
+    private FirebaseAuth mAuth;
 
-    private FirebaseAuth mAuth ;
+    private ViewPager mViewPager;
+    private SectionsPagerAdapter mSectionsPager;
+
+    private TabLayout mTabLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.main_tabPager);
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mSectionsPager = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPager);
 
         mAuth = FirebaseAuth.getInstance();
-
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("MyChatApp");
     }
 
     public void onStart() {
@@ -33,5 +52,25 @@ public class MainActivity extends AppCompatActivity {
         Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
         startActivity(startIntent);
         finish();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.main_logout_btn) {
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        } else if (item.getItemId() == R.id.main_settings_button) {
+            Intent account_intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(account_intent);
+        }
+
+        return true;
     }
 }
--- MyChatApp/app/src/main/java/com/example/adityaparulekar/mychatapp/StartActivity.java
+++ MyChatApp/app/src/main/java/com/example/adityaparulekar/mychatapp/StartActivity.java
@@ -1,17 +1,37 @@
 package com.example.adityaparulekar.mychatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
     private Button nRegBtn;
+    private Button nLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
         nRegBtn = (Button) findViewById(R.id.start_reg_btn);
+        nLoginBtn = (Button) findViewById(R.id.start_login_btn);
+        nRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
+        nLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login_intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(login_intent);
+            }
+        });
     }
 }
